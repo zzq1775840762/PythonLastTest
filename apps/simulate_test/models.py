7cc495de73ac7b751c492eb848dc2eb6c3dcab14
zzq1775840762 <1775840762@qq.com>
2021-06-07 18:55:45 +0800

    2021/06/07 18:51 创建超级用户，admin，密码123456

--- apps/simulate_test/models.py
+++ apps/simulate_test/models.py
@@ -45,9 +45,7 @@ class Test(models.Model):
         verbose_name_plural = verbose_name
 
     def __str__(self):
-        if self.option:
-            return self.option
-        return self.id
+        return '{0}回答{1}'.format(self.tuser,self.tproblem)
 
 
 class Result(models.Model):
